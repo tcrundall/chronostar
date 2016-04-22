e66af08145c9e3ce9664e7899ccfa638abd873e9
Tim Crundall <tim.crundall@gmail.com>
2016-04-22 23:41:40 +1000

    Using Eigen to generate random matrices

---
+++ benchmarks/eigDeterm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/LU>
+#include <Eigen/Core>
+
+using namespace std;
+using namespace Eigen;
+
+int main(int argc, char * argv[])
+{
+  if (argc != 3 || strcmp(argv[1], "-h") == 0)
+  {
+    cout << "Usage: ./eigDeterm N_MATRIX(<=1000) ITERATIONS(~1000)"
+      << endl;
+    return 1;
+  }
+
+  srand(time(NULL));
+
+  //MatrixXd m = MatrixXd::Random(6, 6);
+  cout << "Here is the matrix m:" << endl << MatrixXd::Random(6,6) << endl;
+  /*Matrix3d inverse;
+  bool invertible;
+  double determinant;
+  m.computeInverseAndDetWithCheck(inverse,determinant, invertible);
+  cout << "Its determinant is " << determinant << endl;
+  if (invertible) {
+    cout << "It is invertible, and its inverse is:" << endl << inverse << endl;
+  }
+  else {
+    cout << "It is not invertible." << endl;
+  }
+*/
+  return 0;
+}
