952e31b46e11256eca1cb907c3a978874cd5f691
Michael Ireland <mireland@malice.anu.edu.au>
2016-05-06 19:06:39 +1000

    Neatened up multiple architecture Makefiles in the style of CHARA.

--- playground/swig/overlap.c
+++ playground/swig/overlap.c
@@ -18,14 +18,14 @@ double get_det(PyObject *A)
   gsl_permutation *p;
 
   p = gsl_permutation_alloc(m->size1);
-
+  
   for (i=0; i<nInts; i++)
   {
     PyObject *oo = PyList_GetItem(A, i);
     //printf("%6.2f\n",PyFloat_AS_DOUBLE(oo));
     gsl_matrix_set (m, i%MAT_DIM, i/MAT_DIM, PyFloat_AS_DOUBLE(oo));
   }
-
+  
   gsl_linalg_LU_decomp(m, p, &signum);
   det = gsl_linalg_LU_det(m, signum);
   //printf("%6.2f\n",det);
