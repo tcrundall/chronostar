8fbe3f0fa7039acb384d6676b9be5433affeb95e
Michael Ireland <mireland@malice.anu.edu.au>
2016-05-13 15:59:48 +1000

    Started up with symmetric matrices

--- playground/swig/overlap_test.py
+++ playground/swig/overlap_test.py
@@ -3,9 +3,9 @@ from __future__ import print_function, division
 from timeit import Timer
 import argparse
 
-t_gsl = Timer('result = overlap.get_overlap(A.flatten().tolist(),a.flatten().tolist(),A_det,B.flatten().tolist(),b.flatten().tolist(),B_det)','import overlap\nimport numpy as np\nA = np.random.random( (6,6) )\na = np.random.random(6)\nA_det = abs(np.linalg.det(A))\nB = np.random.random( (6,6) )\nb = np.random.random(6)\nB_det = abs(np.linalg.det(B))')
+t_gsl = Timer('result = overlap.get_overlap(A.flatten().tolist(),a.flatten().tolist(),A_det,B.flatten().tolist(),b.flatten().tolist(),B_det)','import overlap\nimport numpy as np\nA = np.random.random( (6,6) )\nA += A.T\na = np.random.random(6)\nA_det = abs(np.linalg.det(A))\nB = np.random.random( (6,6) )\nB += B.T\nb = np.random.random(6)\nB_det = abs(np.linalg.det(B))')
 
-t_np = Timer('overlap = fit_group_reduced.compute_overlap(A,a,A_det,B,b,B_det)','import numpy as np\nimport fit_group_reduced\nA = np.random.random( (6,6) )\na = np.random.random(6)\nA_det = abs(np.linalg.det(A))\nB = np.random.random( (6,6) )\nb = np.random.random(6)\nB_det = abs(np.linalg.det(B))')
+t_np = Timer('overlap = fit_group_reduced.compute_overlap(A,a,A_det,B,b,B_det)','import numpy as np\nimport fit_group_reduced\nA = np.random.random( (6,6) )\nA += A.T\na = np.random.random(6)\nA_det = abs(np.linalg.det(A))\nB = np.random.random( (6,6) )\nB += B.T\nb = np.random.random(6)\nB_det = abs(np.linalg.det(B))')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--iterations', dest='iter', help='number of iterations')
