28c92c6eea5dadfd5448ca4d0cd47932715bfd01
JonahHansen <hansen.jonah833@gmail.com>
2016-09-23 00:37:35 +1000

    Duplicate/Binary script works

--- chronostar/duplicate_binary.py
+++ chronostar/duplicate_binary.py
@@ -7,58 +7,67 @@ Created on Sat Sep 17 00:39:03 2016
 
 from astropy.table import Table
 import numpy as np
+import math
 
 #Read in table
-t = Table.read('filename.csv', format = 'ascii')
+t = Table.read('gdoc_Torres.csv', format = 'ascii')
 #Sort table according to RA and DE
 t.sort(['RAdeg', 'DEdeg'])
 
 #Choose the maximum difference in angular position for binary stars (deg)
 bin_dist = 0.01
-arcs = 0.000278
+#Choose the maximum difference in angular position to identify duplicates eg arcsecond (deg)
+arcs = 0.0001
 
-new_t=t
 i=0
+rmv = []
 
-while i < len(t):
-    d_b = []
+#Remove Duplicates
+while i < (len(t)-1):
     l = 1
-    
+  
     #Determines if a star and the next in the table are within an arcsec of 
     #each other. That is, they are identical.
-    if abs(t['RAdeg'][i+1] - t['RAdeg'][i]) < arcs and abs(t['DEdeg'][i+1] - t['DEdeg'][i]) < arcs:
-        d_b = np.append(d_b,t[i:i+2], axis = 0)
+    if (math.fabs(t['RAdeg'][i+1] - t['RAdeg'][i]) < arcs and math.fabs(t['DEdeg'][i+1] - t['DEdeg'][i]) < arcs) or t['Name1'][i+1] == t['Name1'][i]:
+        duplicates = t[i:i+2]
         n= i+1
         #Searches for other identical stars
-        while abs(t['RAdeg'][n+1] - t['RAdeg'][n]) < arcs and abs(t['DEdeg'][n+1] - t['DEdeg'][n]) < arcs:
-            d_b = np.append(d_b,t[n+1], axis = 0)
+        while math.fabs(t['RAdeg'][n+1] - t['RAdeg'][n]) < arcs and math.fabs(t['DEdeg'][n+1] - t['DEdeg'][n]) < arcs:
+            duplicates.add_row(t[n+1])
             n += 1
-        l = len(d_b)
+        l = len(duplicates)
         
         #Averages the coordinates and finds the weighted average RV
-        new_t['RA'][i] = np.mean(d_b['RAdeg'])
-        new_t['DE'][i] = np.mean(d_b['DEdeg'])
-        sum_w = np.sum(1/(d_b['RVsig'])**2)
-        new_t['RV'][i] = np.sum(d_b['RV']*(1/(d_b['RVsig'])**2))/sum_w
-        new_t['RVsig'][i] = 1/np.sqrt(sum_w)
-        new_t = np.delete(new_t, range(i+1,i+(len(d_b))), axis = 0)
-        
+        t['RAdeg'][i] = np.mean(duplicates['RAdeg'])
+        t['DEdeg'][i] = np.mean(duplicates['DEdeg'])
+        sum_w = np.sum(1/(duplicates['RV error'])**2)
+        t['RV'][i] = np.sum(duplicates['RV']*(1/(duplicates['RV error'])**2))/sum_w
+        t['RV error'][i] = 1/np.sqrt(sum_w)
+        rmv = np.append(rmv, range(i+1,i+(len(duplicates))))
+                
+    i += l
+
+t.remove_rows(rmv)
+
+#Find Binaries and average RVs
+while i < (len(t)-1):
+    l = 1    
     #Determines if a star and the next one are within a certain max distance
     #of each other (Designed to locate binary systems)
-    elif abs(t['RAdeg'][i+1] - t['RAdeg'][i]) < bin_dist and abs(t['DEdeg'][i+1] - t['DEdeg'][i]) < bin_dist:
-        d_b = np.append(d_b,(t[i:i+2]), axis = 0)
-        l = len(d_b)
+    if math.fabs(t['RAdeg'][i+1] - t['RAdeg'][i]) < bin_dist and math.fabs(t['DEdeg'][i+1] - t['DEdeg'][i]) < bin_dist:
+        binaries = t[i:i+2]
+        l = len(binaries)
         
         #Averages the RV
-        new_t['RV'][i:i+2] = np.mean(d_b['RV'])
+        t['RV'][i:i+2] = np.mean(binaries['RV'])
         
         #Performs the RVerr calc, as described on Slack
-        w_err = 1/np.sqrt(np.sum(1/(d_b['RVsig'])**2))
+        w_err = 1/np.sqrt(np.sum(1/(binaries['RV error'])**2))
         
         #Updates both binary stars with new RVs, but same coordinates
-        new_t['RVsig'][i:i+2] = np.sqrt(w_err**2+(np.diff(d_b['RVsig'])/4)**2)
+        t['RV error'][i:i+2] = np.sqrt(w_err**2+(np.diff(binaries['RV error'])/4)**2)
         
     i += l
 
 #Writes the modified table to either the same, or new file
-new_t.write('filename.csv', format='ascii')
+t.write('gdoc_mod.csv', format='ascii')
