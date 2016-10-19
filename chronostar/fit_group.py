Fixed covariance matrix in overlap (now symmetrical!)

chronostar/fit_group.py
@@ -63,7 +63,8 @@ def compute_overlap(A,a,A_det,B,b,B_det):
     #Compute the overlap formula.
     overlap = np.exp(-0.5*(np.dot(b-c,np.dot(B,b-c)) + \
                            np.dot(a-c,np.dot(A,a-c)) )) 
-    overlap *= np.sqrt(B_det*A_det/ApB_det)/(2*np.pi)**3.0
+
+    #overlap *= np.sqrt(B_det*A_det/ApB_det)/(2*np.pi)**3.0
     
     return overlap
    

test_betapic_TGAS.py
@@ -10,8 +10,8 @@ import chronostar.traceback as traceback
 import chronostar.fit_group as fit_group
 #plt.ion()
 
-trace_it_back = True
-fit_the_group = False
+trace_it_back = False
+fit_the_group = True
 n_times = 31
 max_time = 30
 
@@ -52,8 +52,14 @@ if fit_the_group:
     
     beta_pic_group = np.array([-6.574, 66.560, 23.436, -1.327,-11.427, -6.527,\
         10.045, 10.319, 12.334,  0.762,  0.932,  0.735,  0.846, 20.589])
+ 
+    beta_pic_group = np.array([-6.574, 66.560, 23.436, -1.327,-11.427, 0,\
+     10.045, 10.319, 12.334,  5,  0.932,  0.735,  0.846, 20.589])
 
-    fitted_group = fit_group.fit_one_group(star_params, init_mod=beta_pic_group,\
-        nwalkers=30,nchain=100,nburn=20, return_sampler=False,pool=None,\
-        init_sdev = np.array([1,1,1,1,1,1,1,1,1,.01,.01,.01,.1,1]), background_density=2e-12, use_swig=False, \
-        plotit=True)
+    ol_swig = fit_group.lnprob_one_group(beta_pic_group, star_params, use_swig=True, return_overlaps=True)
+    ol_old  = fit_group.lnprob_one_group(beta_pic_group, star_params, use_swig=False, return_overlaps=True)
+
+#    fitted_group = fit_group.fit_one_group(star_params, init_mod=beta_pic_group,\
+#        nwalkers=30,nchain=100,nburn=20, return_sampler=False,pool=None,\
+#        init_sdev = np.array([1,1,1,1,1,1,1,1,1,.01,.01,.01,.1,1]), background_density=2e-12, use_swig=False, \
+#        plotit=True)