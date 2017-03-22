f4e630ba4a2bf0f5a21725bbe147c93a3805c97d
Tim Crundall <tim.crundall@gmail.com>
2017-03-22 12:20:00 +1100

    Realign samples is written and tested

--- chronostar/analyser.py
+++ chronostar/analyser.py
@@ -23,6 +23,7 @@ import pdb          # for debugging
 import corner       # for pretty corner plots
 import pickle       # for dumping and reading data
 import numpy as np
+from sympy.utilities.iterables import multiset_permutations
 
 def read_sampler(infile='logs/gf_bp_2_3_10_10.pkl'):
     """
@@ -33,14 +34,124 @@ def read_sampler(infile='logs/gf_bp_2_3_10_10.pkl'):
 
     return chain, lnprob, pos, nfree, nfixed, nsteps, npars
 
-def realign_samples():
+def realign_samples(flatchain, flatlnprob, nfree, nfixed, npars):
     """
     given a set of samples, realign sample by finding the permutation
     of each sample's group parameters such that the groups in the 
     permuted sample are the closest they can be to the
     groups represented by the sample with the highest lnprob
     """
-    return 0
+    # Initialising array for realigned samples, extra space for weight
+    # to be derived
+    # GET MIKE TO HELP WORK OUT HOW TO SIMPLY APPEND EXTRA PARAMETER
+    # USING NP.APPEND OR SOME SHIT
+    realigned_samples = np.zeros((np.shape(flatchain)[0],
+                                  np.shape(flatchain)[1]+1))
+    ngroups = nfree+nfixed
+
+    # no need to realign anything if there's only the one group
+    if ngroups == 0:
+        return flatchain
+
+    for i, sample in enumerate(flatchain):
+        weights = sample[-(ngroups-1):]
+        derived_weight = 1 - np.sum(weights)
+        realigned_samples[i] = np.append(sample, derived_weight)
+
+    best_ix = np.argmax(flatlnprob)
+    best_sample = realigned_samples[best_ix]
+
+    permute_count = 0
+
+    for i, sample in enumerate(realigned_samples):
+        realigned_sample = permute(sample, best_sample, nfree, nfixed)
+        if np.array_equal(realigned_samples[i],realigned_sample):
+            permute_count += 1
+        realigned_samples[i] = realigned_sample
+    
+    return realigned_samples, permute_count
+
+def permute(sample, best_sample, nfree, nfixed):
+    """
+    Takes a sample and the best_sample (the one with the highest
+    lnprob) and returns the perumtation of sample which is the closest
+    to best_sample.
+
+    Input samples should already have derived weight appeneded.
+
+    The reason this is necessary is because, if sample has more than one
+    free group, there is nothing distinguishing which group is character-
+    ised by which parameters
+    """
+    npars_wo_amp = 14
+    assert(np.size(best_sample) == npars_wo_amp * nfree + (nfree + nfixed))
+    free_groups = np.reshape(  sample[:npars_wo_amp * nfree],(nfree,-1))
+    best_fgs = np.reshape(best_sample[:npars_wo_amp * nfree],(nfree,-1))
+
+    if (nfixed != 0):
+        free_amps   = sample[-(nfree + nfixed):-nfixed]
+        fixed_amps  = sample[-nfixed:]
+        best_fas = best_sample[-(nfree + nfixed):-nfixed]
+        best_xas = best_sample[-nfixed:]
+    else:
+        # Special behaviour if all groups are free
+        free_amps   = sample[-nfree:]
+        fixed_amps  = sample[:0]        # an empty array
+        best_fas = best_sample[-nfree:]
+        best_xas = best_sample[:0]      # an empty array
+
+    # try using the np.fromfunction here?
+    Dmat = np.zeros((nfree,nfree))
+    for i in range(nfree):
+        for j in range(nfree):
+            Dmat[i,j] = group_metric(free_groups[i], best_fgs[j])
+
+    ps = [p for p in multiset_permutations(range(nfree))]
+    traces = [np.trace(Dmat[p]) for p in ps]
+    best_perm = ps[np.argmin(traces)]
+
+    # ... should I rearrange the layout of the parameters?
+    perm_sample = np.append(np.append(free_groups[best_perm],
+                                      free_amps[best_perm]),
+                            fixed_amps)
+    try:
+        assert(np.size(perm_sample) == np.size(sample)),\
+                "Wrong size...\n{}\n{}".format(sample, perm_sample)
+    except:
+        pdb.set_trace()
+
+    return perm_sample
+
+def group_metric(group1, group2):
+    """
+    Calculates the metric distance between two groups.
+    Inputs are two np.arrays of size 14.
+    Note that the group inputs are raw parametes, that is stds are
+    parametrised as 1/std in the emcee chain so must be inverted
+    """
+    means1 = group1[:6];      means2 = group2[:6]
+    stds1  = 1/group1[6:10];  stds2  = 1/group2[6:10]
+    corrs1 = group1[10:13];   corrs2 = group2[10:13]
+    age1   = group1[13];      age2   = group2[13]
+
+    total_dist = 0
+    for i in range(3):
+        total_dist += (means1[i] - means2[i])**2 /\
+                        (stds1[i]**2 + stds2[i]**2)
+
+    for i in range(3,6):
+        total_dist += (means1[i] - means2[i])**2 /\
+                        (stds1[3]**2 + stds2[3]**2)
+
+    for i in range(4):
+        total_dist += (np.log(stds1[i] / stds2[i]))**2
+
+    for i in range(3):
+        total_dist += (corrs1[i] - corrs2[i])**2
+
+    total_dist += (np.log(age1/age2))**2
+
+    return np.sqrt(total_dist)
 
 def calc_bet_fit():
     """
--- test_analyser.py
+++ test_analyser.py
@@ -9,5 +9,18 @@ assert(read_sampler(samples_file) != 0), "Read_sampler not functioning"
 
 chain, lnprob, pos, nfree, nfixed, nsteps, npars  = read_sampler(samples_file)
 assert(np.size(chain) != 0), "Chain read in as zero"
+nwalkers = np.shape(chain)[0]
+
+flatchain = np.reshape(chain, (nwalkers*nsteps,npars))
+flatlnprob = np.reshape(lnprob, (nwalkers*nsteps))
 
 assert(nfree == 3 and nfixed == 2)
+
+realigned_samples, permute_count = realign_samples(flatchain, flatlnprob,
+                                                   nfree, nfixed, npars)
+
+print("Permutations: {} out of: {}".format(permute_count,
+                                           np.shape(flatchain)[0]) )
+assert(np.shape(realigned_samples)[1] == np.shape(flatchain)[1]+1),\
+            "Difference: {}".format(np.shape(realigned_samples)[1] -\
+                                    np.shape(flatchain)[1])
