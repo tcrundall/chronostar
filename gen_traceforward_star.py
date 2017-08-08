2eb51082d0b0c69090925915b57455f38dfe77a4
Tim Crundall <tim.crundall@gmail.com>
2017-08-08 14:10:34 +1000

    Bug tester for trace_forward written

--- gen_traceforward_star.py
+++ gen_traceforward_star.py
@@ -4,25 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pdb
 
-s = [9.27,-5.96,-13.59,-10.94,-16.25,-9.27]
+#s = [9.27,-5.96,-13.59,-10.94,-16.25,-9.27]
+s = [0,0,0,0,0,0]
 
 nts = 10
-max_age = 2
+max_age = 10
 age_step = max_age*1.0 / nts
-ts = np.linspace(0.01,max_age,nts)
+ts = np.linspace(1,max_age,nts)
 xyzuvw = np.zeros((nts,6))
-xyzuvw[0] = s
-
+xyzuvw[0]  = s
 xyzuvw2 = np.zeros((nts,6))
+xyzuvw2[0] = s
 
-# for some reason, doing it recursively seems to double add
-# the  [0,0,25,11.1,12.24,7.25] (xyzuvw_sun) to the xyzuvw of the star...
+pdb.set_trace()
+
+# BUG!!! For whatever reason, the solar motion is being added to the end
+# result twice...
+
+# This loop iteratively builds a list of positions by tracing forward from 
+# the previously calculated position
 for i in range(nts-1):
-    pdb.set_trace()
+    #pdb.set_trace()
     xyzuvw[i+1] = tb.trace_forward(xyzuvw[i], age_step)
 
-for i, age in enumerate(ts):
-    xyzuvw2[i] = tb.trace_forward(s, age)
+# This loop calculates the positions by tracing forward from the initial
+# posiiton each time
+for i, age in enumerate(ts[1:]):
+    xyzuvw2[i+1] = tb.trace_forward(s, age)
+
 #pdb.set_trace()
 plt.plot(xyzuvw[:,0], xyzuvw[:,1])
 plt.plot(xyzuvw2[:,0], xyzuvw2[:,1])
