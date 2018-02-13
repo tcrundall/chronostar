9fe8913cbe220ef9ac470e98404dcff19b65e8e7
Tim Crundall <tim.crundall@gmail.com>
2018-02-13 19:25:59 +1100

    Storing result as numpy save

--- scripts/investigator_demo.py
+++ scripts/investigator_demo.py
@@ -7,6 +7,7 @@ import chronostar.investigator as iv
 import pdb
 import numpy as np
 import logging
+import pickle
 
 #logging.basicConfig(
 #    format='%(asctime)s %(message)s',
@@ -14,19 +15,30 @@ import logging
 #)
 
 
+save_dir = '../data/synth_trial/'
 
-logging.basicConfig(filename='investigator_demo.log', level=logging.DEBUG)
-logging.info('Started')
+logging.basicConfig(
+    filename=save_dir+'investigator_demo.log', level=logging.DEBUG,
+    filemode='w'
+)
+logging.info('----- Started -----')
 mock_group_pars =\
     [20, 20, 5, -22, -10, -3, 5, 5, 5, 3, 0.5, 0.5, 0.3, 10, 10]
 times = np.linspace(0,20,5)
 fixed_ages = np.linspace(0,20,3)
 
-mysf = iv.SynthFit(mock_group_pars, times=times, fixed_ages=fixed_ages)
+logging.info('__Initialising__')
+mysf = iv.SynthFit(
+    mock_group_pars, times=times, fixed_ages=fixed_ages, save_dir=save_dir
+)
+logging.info('__Investigating__')
 mysf.investigate()
+save_file = save_dir + 'synth_result'
+np.save(save_file, mysf)
 
-pdb.set_trace()
-
-logging.info('Finished')
+logging.info('__Storing__')
+yoursf = np.load(save_file+'.npy').item()
+logging.info('fixed_ages attribute from storage: {}'.format(yoursf.fixed_ages))
+logging.info('----- Finished -----')
 
 
