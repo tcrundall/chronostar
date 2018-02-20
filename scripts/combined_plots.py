d805d682e777c5bc2b17a9ffa7edc957f361c644
Timothy Crundall <u5018130@anu.edu.au>
2018-02-20 11:26:01 +1100

    Minor edits

---
+++ scripts/combined_plots.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+"""Takes the resulting directory of exhaustive_traceback_analysis.py
+and plots a bunch of features from the synthfit.npy saves
+"""
+
--- scripts/exhaustive_traceback_analysis.py
+++ scripts/exhaustive_traceback_analysis.py
@@ -5,12 +5,12 @@ their results in a hierarchical directory structure.
 
 try:
     import matplotlib as mpl
+
     mpl.use('Agg')
     import matplotlib.pyplot as plt
 except ImportError:
     print("Warning: matplotlib not imported")
     pass
-
 
 from distutils.dir_util import mkpath
 from multiprocessing import Pool
@@ -26,14 +26,14 @@ import chronostar.investigator as iv
 import chronostar.quadplotter as qp
 
 SAVE_DIR = '../results/synth_results/'
-NTIMES = 41 
-NFIXED_FITS = 21 
+NTIMES = 41
+NFIXED_FITS = 21
 NSTEPS = 2000
 
 ages = [5, 10, 20]
 spreads = [2, 5, 10]
 v_disps = [2, 5]
-sizes   = [25, 50]
+sizes = [25, 50]
 precs = ['perf', 'gaia']
 """
 ages = [15]
@@ -48,9 +48,10 @@ base_group_pars = [
     0.0, 0.0, 0.0, None, None
 ]
 
-prec_val = {'perf':1e-5, 'gaia':1.0, 'double':2.0}
+prec_val = {'perf': 1e-5, 'gaia': 1.0, 'double': 2.0}
 
-def do_something(age,spread,v_disp,size,prec):
+
+def do_something(age, spread, v_disp, size, prec):
     path_name = SAVE_DIR + "{}_{}_{}_{}_{}/".format(
         age, spread, v_disp, size, prec
     )
@@ -65,23 +66,20 @@ def do_something(age,spread,v_disp,size,prec):
     group_pars[13] = age
     group_pars[14] = size
 
-    times = np.linspace(0, 2*age, NTIMES)
+    times = np.linspace(0, 2 * age, NTIMES)
 
     sf = iv.SynthFit(init_group_pars=group_pars, save_dir=path_name,
-                times=times, nfixed_fits=NFIXED_FITS, prec=prec_val[prec])
+                     times=times, nfixed_fits=NFIXED_FITS, prec=prec_val[prec])
     sf.investigate(period=NSTEPS)
-    np.save(path_name+"synthfit.npy", sf)
+    np.save(path_name + "synthfit.npy", sf)
     qp.quadplot_synth_res(sf, save_dir=path_name)
-#    os.remove(sf.perf_data_file)
-#    os.remove(sf.gaia_data_file)
-#    os.remove(sf.perf_tb_file)
-#    os.remove(sf.gaia_tb_file)
 
 
 def do_something_wrapper(scenario):
     print("In wrapper")
     print(".. scenario: {}".format(scenario))
     do_something(*scenario)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
