3520e77ba83ed0299e2b0f77c06158c6014dbd5e
Timothy Crundall <u5018130@anu.edu.au>
2018-05-11 23:27:14 +1000

    More bugs

--- chronostar/expectmax.py
+++ chronostar/expectmax.py
@@ -346,7 +346,7 @@ def fitManyGroups(star_pars, ngroups, rdir='', init_z=None,
         logging.info("Membership distribution:\n{}".format(
             z.sum(axis=0)
         ))
-        if (min(z.sum(axis=0)) < (0.01 * z.sum)):
+        if (min(z.sum(axis=0)) < (0.01 * z.sum())):
             logging.info("!!! WARNING, GROUP {} HAS LESS THAN 1% OF STARS".\
                          format(np.argmin(z.sum(axis=0))))
         np.save(idir+"membership.npy", z)
