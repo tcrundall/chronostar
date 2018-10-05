eb4929fe61838b6fb6632c1ac0627593d10f1936
Timothy Crundall <u5018130@anu.edu.au>
2018-10-05 11:52:39 +1000

    Minor log changes

--- scripts/perform_incremental_em_synth_fit.py
+++ scripts/perform_incremental_em_synth_fit.py
@@ -328,7 +328,7 @@ while ncomps < MAX_COMP:
         logging.info('Final age med and span:')
         [logging.info(row[-1] for row in prev_meds)]
         logging.info('Final membership:')
-        logging.info(prev_z)
+        logging.info(np.round(prev_z*100), decimals=2)
         logging.info('Final lnlikelihood: {}'.format(prev_lnlike))
         logging.info('Final lnposterior:  {}'.format(prev_lnpost))
         logging.info('Final BIC: {}'.format(prev_BIC))
