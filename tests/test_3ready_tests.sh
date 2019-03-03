595e213f9080ea22983cda6512b301b256c15f0d
Timothy Crundall <u5018130@anu.edu.au>
2019-03-03 12:31:41 +1100

    Warp mpirun test neatly

--- tests/test_3ready_tests.sh
+++ tests/test_3ready_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 -m pytest test_coordinate.py test_transform.py test_traceorbit.py test_component.py test_synthdata.py
+python3 -m pytest test_coordinate.py test_transform.py test_traceorbit.py test_component.py test_synthdata.py test_mpi.py
