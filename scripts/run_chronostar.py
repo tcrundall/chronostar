baf19eb8c25c9da7c509c0ba62703a8abbf390a2
Timothy Crundall <u5018130@anu.edu.au>
2019-04-18 15:06:11 +0200

    Explicitly checks for mpi flag, fix bug with Component init call

--- scripts/run_chronostar.py
+++ scripts/run_chronostar.py
@@ -88,25 +88,28 @@ logging.basicConfig(filename=rdir+'log.log', level=logging.INFO)
 # ------------------------------------------------------------
 # -----  BEGIN MPIRUN THING  ---------------------------------
 # ------------------------------------------------------------
-using_mpi = False # True
-try:
-    pool = MPIPool()
-    logging.info("Successfully initialised mpi pool")
-except:
-    #print("MPI doesn't seem to be installed... maybe install it?")
-    logging.info("MPI doesn't seem to be installed... maybe install it?")
-    using_mpi = False
-    pool=None
 
-if using_mpi:
-    if not pool.is_master():
-        print("One thread is going to sleep")
-        # Wait for instructions from the master process.
-        pool.wait()
-        sys.exit(0)
-print("Only one thread is master")
+# Only even try to use MPI if config file says so
+if config.config.get('run_with_mpi', False):
+    using_mpi = False # True
+    try:
+        pool = MPIPool()
+        logging.info("Successfully initialised mpi pool")
+    except:
+        #print("MPI doesn't seem to be installed... maybe install it?")
+        logging.info("MPI doesn't seem to be installed... maybe install it?")
+        using_mpi = False
+        pool=None
 
-
+    if using_mpi:
+        if not pool.is_master():
+            print("One thread is going to sleep")
+            # Wait for instructions from the master process.
+            pool.wait()
+            sys.exit(0)
+    print("Only one thread is master")
+else:
+    print("MPI flag was not set in config file")
 
 log_message('Beginning Chronostar run',
             symbol='_', surround=True)
@@ -278,7 +281,7 @@ try:
             npars = len(Component.PARAMETER_FORMAT)
             best_ix = np.argmax(lnprob)
             best_pars = chain.reshape(-1,npars)[best_ix]
-            prev_comps[i] = Component(pars=best_pars, internal=True)
+            prev_comps[i] = Component(emcee_pars=best_pars)
         np.save(str(run_dir+'final/'+final_comps_file), prev_comps)
 
     logging.info('Loaded from previous run')
