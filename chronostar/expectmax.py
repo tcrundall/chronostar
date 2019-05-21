a53389d1cbb17a5989170e056affbe5ac92549de
Timothy Crundall <u5018130@anu.edu.au>
2019-05-21 11:29:56 +0200

    Undoing previous commit

--- chronostar/expectmax.py
+++ chronostar/expectmax.py
@@ -111,7 +111,7 @@ def get_background_covariance(data, amp_scale=1.0):
 
     kernel = stats.gaussian_kde(data.T)
     #print('KERNEL.COVARIANCE', kernel.covariance) # MZ
-    points = np.copy(data)
+    points = np.copy(points)
     points[:,2] *= -1
     points[:,5] *= -1
 
