b443d6814a12c5561354eb3dca7cce01e524c905
marusazerjal <marusa.zerjal@anu.edu.au>
2019-05-26 21:05:23 +1000

    Run with small errors

--- chronostar/expectmax.py
+++ chronostar/expectmax.py
@@ -118,41 +118,14 @@ def get_background_overlaps_with_covariances(kernel_density_input_datafile, data
 
     # shapes of the c_get_lnoverlaps input must be: (6, 6), (6,), (120, 6, 6), (120, 6)
     # So I do it in a loop for every star
-    bg_lnols=[]
-    i=0
-    for star_mean, star_cov in zip(star_means, star_covs):
-        bg_lnol = c_get_lnoverlaps(star_cov, star_mean, background_covs, background_means, nstars)
-        #mask = bg_lnol > -800
-        #bg_lnol = bg_lnol[mask]
-        #print('bg_lnol', len(bg_lnol[mask]), bg_lnol[mask])
-        #mi=np.min(bg_lnol)
-        #ma=np.max(bg_lnol)
-        #print(mi, ma, mi-ma)
-
-        # An alternative approach
-        #if np.any(mask):
-        #    bg_lnol_sorted = sorted(bg_lnol[mask])
-        #    x0=bg_lnol_sorted[-1]
-        #    diff=bg_lnol_sorted[:-1]-x0
-        #    result = x0 + np.log(1.0 + np.sum(np.exp(diff)))
-        #else:
-        #    result=0
-
-        #try:
-        bg_lnol = np.log(np.sum(np.exp(bg_lnol))) # sum in linear space
-        #except:
-        #    bg_lnol=np.inf
-        print(i, 'bg_lnol', bg_lnol)
-        #print('')
-        bg_lnols.append(bg_lnol)
-        i+=1
-
-        # debugging
-        if i>10:
-            break
+    #bg_lnols=[]
+    #for star_mean, star_cov in zip(star_means, star_covs):
+    #    bg_lnol = c_get_lnoverlaps(star_cov, star_mean, background_covs, background_means, nstars)
+    #    bg_lnol = np.log(np.sum(np.exp(bg_lnol))) # sum in linear space
+    #    bg_lnols.append(bg_lnol)
 
     # This should be parallelized
-    #bg_lnols = [np.sum(c_get_lnoverlaps(star_cov, star_mean, background_covs, background_means, nstars)) for star_mean, star_cov in zip(star_means, star_covs)]
+    bg_lnols = [np.sum(c_get_lnoverlaps(star_cov, star_mean, background_covs, background_means, nstars)) for star_mean, star_cov in zip(star_means, star_covs)]
     #print(bg_lnols)
 
     return bg_lnols
