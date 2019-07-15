f1b869cb534c1adfcd4b8a956d1e91acc576db9e
marusazerjal <marusa.zerjal@anu.edu.au>
2019-07-15 23:11:41 +1000

    Bg overlaps in ScoCen with missing RV

--- scocen/component_overlap_for_stars_with_missing_RV.py
+++ scocen/component_overlap_for_stars_with_missing_RV.py
@@ -17,62 +17,76 @@ from chronostar import tabletool
 from chronostar import expectmax
 from astropy.table import Table, vstack, join
 
-# Read all components
-c_usco = np.load('usco_res/final_comps.npy')
-c_ucl = np.load('ucl_res/final_comps.npy')
+datafile = 'data_table_cartesian.fits'
 
-c = np.vstack((c_usco, c_ucl))
+
+# Read all components
+#c_usco = np.load('usco_res/final_comps.npy')
+#c_ucl = np.load('ucl_res/final_comps.npy')
+#c = np.vstack((c_usco, c_ucl))
+
+c = np.load('all_nonbg_scocen_comps.npy') # including LCC
+print('components', c.shape)
+print('Are there duplicate components?')
+
 print (c)
 print (c.shape)
 
-# Read Gaia data including both stars with known and missing radial velocities
-data_table = tabletool.read('../data/ScoCen_box_result.fits')
-#data_table = tabletool.read('../data/scocen_for_testing.fits') # Shorter table
-#data_table=data_table[:100]
-print('DATA READ', len(data_table))
-# Set missing radial velocities to some value, and their errors to something very big
-
-# Set missing radial velocities (nan) to 0
-data_table['radial_velocity'] = np.nan_to_num(data_table['radial_velocity'])
-
-# Set missing radial velocity errors (nan) to 1e+10
-data_table['radial_velocity_error'][np.isnan(data_table['radial_velocity_error'])] = 1e+4
-
-print(data_table)
-
-# Convert to Cartesian
-print('Convert to cartesian')
-historical = 'c_XU' in data_table.colnames
-# Performs conversion in place (in memory) on `data_table`
-if (not 'c_XU' in data_table.colnames and not 'X_U_corr' in data_table.colnames):
-    tabletool.convert_table_astro2cart(table=data_table, return_table=True)
 
 
-data_table.write('data_table_cartesian.fits')
+try:
+    data_table = Table.read(datafile)
+except:
+    # Read Gaia data including both stars with known and missing radial velocities
+    data_table = tabletool.read('../data/ScoCen_box_result.fits')
+    #data_table = tabletool.read('../data/scocen_for_testing.fits') # Shorter table
+    #data_table=data_table[:100]
+    print('DATA READ', len(data_table))
+    # Set missing radial velocities to some value, and their errors to something very big
+
+    # Set missing radial velocities (nan) to 0
+    data_table['radial_velocity'] = np.nan_to_num(data_table['radial_velocity'])
+
+    # Set missing radial velocity errors (nan) to 1e+10
+    data_table['radial_velocity_error'][np.isnan(data_table['radial_velocity_error'])] = 1e+4
+
+    print(data_table)
+
+    # Convert to Cartesian
+    print('Convert to cartesian')
+    historical = 'c_XU' in data_table.colnames
+    # Performs conversion in place (in memory) on `data_table`
+    if (not 'c_XU' in data_table.colnames and not 'X_U_corr' in data_table.colnames):
+        tabletool.convert_table_astro2cart(table=data_table, return_table=True)
+
+
+#data_table.write('data_table_cartesian.fits')
 
 # data_table should include background overlaps as well
+
+# Background overlaps (using covariance matrix)
+ln_bg_ols = expectmax.get_background_overlaps_with_covariances(
+    '/home/tcrun/chronostar/data/gaia_cartesian_full_6d_table.fits',
+    data_table,
+)
+
+bg_lnol_colname = 'background_log_overlap'
+print('Background overlaps')
+tabletool.insert_column(data_table, bg_lnol_colname, ln_bg_ols, filename=datafile)
 
 print('Create data dict')
 # Create data dict
 data_dict = tabletool.build_data_dict_from_table(
         data_table,
-        get_background_overlaps=False, # TODO change to True and provide bg ols
+        get_background_overlaps=True, # TODO change to True and provide bg ols
         historical=historical,
 )
-
-
-
-#print (data_dict)
 
 # Create components
 comps = [SphereComponent(pars=x) for x in c]
 print(comps)
 
-# Background overlaps (using covariance matrix)
-#ln_bg_ols = expectmax.get_background_overlaps_with_covariances(
-#    '/home/tcrun/chronostar/data/gaia_cartesian_full_6d_table.fits',
-#    data_table,
-#)
+
 
 overlaps = expectmax.get_all_lnoverlaps(data_dict, comps)
 #print(overlaps)
@@ -82,10 +96,12 @@ membership_probabilities = np.array([expectmax.calc_membership_probs(ol) for ol 
 #print(membership_probabilities)
 #print(membership_probabilities.shape)
 
-for i in range(4):
-    data_table['comp_overlap_usco%d'%(i+1)]=membership_probabilities[:,i]
-for i in range(4):
-    data_table['comp_overlap_ucl%d'%(i+1)]=membership_probabilities[:,i+4]
+for i in range(membership_probabilities.shape[1]):
+    data_table['comp_overlap_%d' % (i + 1)] = membership_probabilities[:, i]
+#for i in range(4):
+#    data_table['comp_overlap_usco%d'%(i+1)]=membership_probabilities[:,i]
+#for i in range(4):
+#    data_table['comp_overlap_ucl%d'%(i+1)]=membership_probabilities[:,i+4]
 
 print(data_table)
 
